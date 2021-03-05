e26347454e469f9e7d7dcdcd6cc909f6e6879e0b
Victor Bak <victor.bak2@gmail.com>
2021-03-04 21:37:50 -0800

    Advanced custom fields and scss

---
+++ wp-content/plugins/wp-scss/options.php
@@ -0,0 +1,300 @@
+<?php
+class Wp_Scss_Settings
+{
+  /**
+   * Holds the values to be used in the fields callbacks
+   */
+  private $options;
+
+  /**
+   * Start up
+   */
+  public function __construct()
+  {
+    add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+    add_action( 'admin_init', array( $this, 'page_init' ) );
+  }
+
+  /**
+   * Add options page
+   */
+  public function add_plugin_page()
+  {
+    // This page will be under "Settings"
+    add_options_page(
+      'Settings Admin',
+      'WP-SCSS',
+      'manage_options',
+      'wpscss_options',
+      array( $this, 'create_admin_page' )
+    );
+  }
+
+  /**
+   * Options page callback
+   */
+  public function create_admin_page()
+  {
+    // Set class property
+    $this->options = get_option( 'wpscss_options' );
+    ?>
+    <div class="wrap">
+        <h2>WP-SCSS Settings</h2>
+        <p>
+          <span class="version">Version <em><?php echo get_option('wpscss_version'); ?></em>
+          <br/>
+          <span class="author">By: <a href="http://connectthink.com" target="_blank">Connect Think</a></span>
+          <br/>
+          <span class="repo">Help & Issues: <a href="https://github.com/ConnectThink/WP-SCSS" target="_blank">Github</a></span>
+        </p>
+        <form method="post" action="options.php">
+        <?php
+            // This prints out all hidden setting fields
+            settings_fields( 'wpscss_options_group' );
+            do_settings_sections( 'wpscss_options' );
+            submit_button();
+        ?>
+        </form>
+    </div>
+    <?php
+  }
+
+  /**
+   * Register and add settings
+   */
+  public function page_init()
+  {
+    register_setting(
+      'wpscss_options_group',    // Option group
+      'wpscss_options',          // Option name
+      array( $this, 'sanitize' ) // Sanitize
+    );
+
+    // Paths to Directories
+    add_settings_section(
+      'wpscss_paths_section',             // ID
+      'Configure Paths',                  // Title
+      array( $this, 'print_paths_info' ), // Callback
+      'wpscss_options'                    // Page
+    );
+
+    add_settings_field(
+      'wpscss_base_folder',                    // ID
+      'Base Location',                         // Title
+      array( $this, 'input_select_callback' ), // Callback
+      'wpscss_options',                        // Page
+      'wpscss_paths_section',                // Section
+      array(                                   // args
+        'name' => 'base_compiling_folder',
+        'type' => apply_filters( 'wp_scss_base_compiling_modes',
+          array(
+            get_stylesheet_directory()  => 'Current theme',
+            wp_get_upload_dir()['basedir'] => 'Uploads directory',
+            WPSCSS_PLUGIN_DIR => 'WP-SCSS Plugin',
+          )
+        )
+      )
+    );
+
+    add_settings_field(
+      'wpscss_scss_dir',                     // ID
+      'SCSS Location',                       // Title
+      array( $this, 'input_text_callback' ), // Callback
+      'wpscss_options',                      // Page
+      'wpscss_paths_section',                // Section
+      array(                                 // args
+        'name' => 'scss_dir',
+      )
+    );
+
+    add_settings_field(
+      'wpscss_css_dir',                       // ID
+      'CSS Location',                         // Title
+      array( $this, 'input_text_callback' ),  // Callback
+      'wpscss_options',                       // Page
+      'wpscss_paths_section',                 // Section
+      array(                                  // args
+        'name' => 'css_dir',
+      )
+    );
+
+    // Compiling Options
+    add_settings_section(
+      'wpscss_compile_section',             // ID
+      'Compiling Options',                  // Title
+      array( $this, 'print_compile_info' ), // Callback
+      'wpscss_options'                      // Page
+    );
+
+    add_settings_field(
+      'Compiling Mode',                        // ID
+      'Compiling Mode',                        // Title
+      array( $this, 'input_select_callback' ), // Callback
+      'wpscss_options',                        // Page
+      'wpscss_compile_section',                // Section
+      array(                                   // args
+        'name' => 'compiling_options',
+        'type' => apply_filters( 'wp_scss_compiling_modes',
+          array(
+            'ScssPhp\ScssPhp\Formatter\Expanded'   => 'Expanded',
+            'ScssPhp\ScssPhp\Formatter\Nested'     => 'Nested',
+            'ScssPhp\ScssPhp\Formatter\Compressed' => 'Compressed',
+            'ScssPhp\ScssPhp\Formatter\Compact'    => 'Compact',
+            'ScssPhp\ScssPhp\Formatter\Crunched'   => 'Crunched',
+            'ScssPhp\ScssPhp\Formatter\Debug'      => 'Debug'
+          )
+        )
+      )
+    );
+
+    add_settings_field(
+      'Source Map Mode',                       // ID
+      'Source Map Mode',                       // Title
+      array( $this, 'input_select_callback' ), // Callback
+      'wpscss_options',                        // Page
+      'wpscss_compile_section',                // Section
+      array(                                   // args
+        'name' => 'sourcemap_options',
+        'type' => apply_filters( 'wp_scss_sourcemap_modes',
+          array(
+            'SOURCE_MAP_NONE'   => 'None',
+            'SOURCE_MAP_INLINE' => 'Inline',
+            'SOURCE_MAP_FILE' => 'File'
+          )
+        )
+      )
+    );
+
+    add_settings_field(
+      'Error Display',                         // ID
+      'Error Display',                         // Title
+      array( $this, 'input_select_callback' ), // Callback
+      'wpscss_options',                        // Page
+      'wpscss_compile_section',                // Section
+      array(                                   // args
+        'name' => 'errors',
+        'type' => apply_filters( 'wp_scss_error_diplay',
+          array(
+            'show'           => 'Show in Header',
+            'show-logged-in' => 'Show to Logged In Users',
+            'hide'           => 'Print to Log',
+          )
+        )
+      )
+    );
+
+    // Enqueuing Options
+    add_settings_section(
+      'wpscss_enqueue_section',             // ID
+      'Enqueuing Options',                  // Title
+      array( $this, 'print_enqueue_info' ), // Callback
+      'wpscss_options'                      // Page
+    );
+
+    add_settings_field(
+      'Enqueue Stylesheets',                     // ID
+      'Enqueue Stylesheets',                     // Title
+      array( $this, 'input_checkbox_callback' ), // Callback
+      'wpscss_options',                          // Page
+      'wpscss_enqueue_section',                  // Section
+      array(                                     // args
+        'name' => 'enqueue'
+      )
+    );
+
+    //  Development options
+    add_settings_section(
+      'wpscss_development_section', // ID
+      'Development Settings',       // Title
+      '',                         // Callback
+      'wpscss_options'            // Page
+    );
+
+    add_settings_field(
+      'wpscss_scss_always_recompile',            // ID
+      'Always Recompile',                        // Title
+      array( $this, 'input_checkbox_callback' ), // Callback
+      'wpscss_options',                          // Page
+      'wpscss_development_section',                // Section
+      array(                                     // args
+        'name' => 'always_recompile',
+      )
+    );
+  }
+
+  /**
+   * Sanitize each setting field as needed
+   *
+   * @param array $input Contains all settings fields as array keys
+   */
+  public function sanitize( $input ) {
+    foreach( ['scss_dir', 'css_dir'] as $dir ){
+      if( !empty( $input[$dir] ) ) {
+        $input[$dir] = sanitize_text_field( $input[$dir] );
+
+        // Add a trailing slash if not already present
+        if(substr($input[$dir], -1) != '/'){
+          $input[$dir] .= '/';
+        }
+      }
+    }
+
+    return $input;
+  }
+
+  /**
+   * Print the Section text
+   */
+  public function print_paths_info() {
+    print 'Location of your SCSS/CSS folders. Folders must be nested under your <b>Base Location</b> and start with <code>/</code>.</br>Examples: <code>/custom-scss/</code> and <code>/custom-css/</code>';
+  }
+  public function print_compile_info() {
+    print 'Choose how you would like SCSS and source maps to be compiled and how you would like the plugin to handle errors';
+  }
+  public function print_enqueue_info() {
+    print 'WP-SCSS can enqueue your css stylesheets in the header automatically.';
+  }
+
+  /**
+   * Text Fields' Callback
+   */
+  public function input_text_callback( $args ) {
+    printf(
+      '<input type="text" id="%s" name="wpscss_options[%s]" value="%s" />',
+      esc_attr( $args['name'] ), esc_attr( $args['name'] ), esc_attr( isset($this->options[$args['name']]) ? $this->options[$args['name']] : '' )
+    );
+  }
+
+  /**
+   * Select Boxes' Callbacks
+   */
+  public function input_select_callback( $args ) {
+    $this->options = get_option( 'wpscss_options' );
+
+    $html = sprintf( '<select id="%s" name="wpscss_options[%s]">', esc_attr( $args['name'] ), esc_attr( $args['name'] ) );
+    foreach( $args['type'] as $value => $title ) {
+      $html .= '<option value="' . esc_attr( $value ) . '"' . selected( isset($this->options[esc_attr( $args['name'] )]) ? $this->options[esc_attr( $args['name'] )] : '', esc_attr( $value ), false) . '>' . esc_attr( $title ) . '</option>';
+    }
+    $html .= '</select>';
+
+    echo $html;
+  }
+
+  /**
+   * Checkboxes' Callbacks
+   */
+  public function input_checkbox_callback( $args ) {
+    $this->options = get_option( 'wpscss_options' );
+    $html = "";
+    if($args['name'] == 'always_recompile' && defined('WP_SCSS_ALWAYS_RECOMPILE') && WP_SCSS_ALWAYS_RECOMPILE){
+      $html .= '<input type="checkbox" id="' . esc_attr( $args['name'] ) . '" name="wpscss_options[' . esc_attr( $args['name'] ) . ']" value="1"' . checked( 1, isset( $this->options[esc_attr( $args['name'] )] ) ? $this->options[esc_attr( $args['name'] )] : 1, false ) . ' disabled=disabled/>';
+      $html .= '<label for="' . esc_attr( $args['name'] ) . '">Currently overwritten by constant <code>WP_SCSS_ALWAYS_RECOMPILE</code></label>';
+    }else{
+      $html .= '<input type="checkbox" id="' . esc_attr( $args['name'] ) . '" name="wpscss_options[' . esc_attr( $args['name'] ) . ']" value="1"' . checked( 1, isset( $this->options[esc_attr( $args['name'] )] ) ? $this->options[esc_attr( $args['name'] )] : 0, false ) . '/>';
+      $html .= '<label for="' . esc_attr( $args['name'] ) . '"></label>';
+    }
+
+
+    echo $html;
+  }
+}
---
+++ wp-content/plugins/wp-scss/readme.md
@@ -0,0 +1,145 @@
+# WP-SCSS
+#### A lightweight SCSS compiler for Wordpress.
+
+Compiles .scss files on your wordpress install using [scssphp](https://github.com/scssphp/scssphp). Includes settings page for configuring directories, error reporting, compiling options, and auto enqueuing.
+
+The plugin only compiles when changes have been made to the scss files. Compiles are made to the matching css file, so disabling this plugin will not take down your stylesheets. In the instance where a matching css file does not exist yet, the plugin will create the appropriate css file in the css directory.
+
+## Settings
+
+#### Directories
+Directories are defined relative to your `base compile folder` which defaults to the theme folder. Alternatly you can choose the uploads directory or plugin directory.  They must be separate from one another, so you cannot define the root folder to compile into itself.
+
+Ideally you should setup a scss folder and a css folder within your theme. This will ensure the most accurate compiling.
+
+    library
+    |-css
+    |  --style.css
+    |  --ie.css
+    |-scss
+    |  --style.scss
+    |  --ie.scss
+
+#### Compiling Mode
+Compiling comes in five modes:
+
+* Expanded - Full open css. One line per property. Brackets close on their own line.
+* Nested - Lightly compressed css. Brackets close with css block. Indents to match scss nesting.
+* Compressed - More compressed css. Entire rule block on one line. No indentation.
+* Compact - Removes all line breaks, unnecessary whitespace, and single-line comments.
+* Crunched - Same as Compressed, but also removes multi-line comments.
+
+See examples of each in [scssphp's documentation](http://scssphp.github.io/scssphp)
+
+#### Source Map Mode
+Source maps come in three modes:
+
+* None - No source map will be generated.
+* Inline - A source map will be generated in the compiled CSS file.
+* File - A source map will be generated as a standalone file in the compiled CSS directory.
+
+#### Error Display
+'Show in Header' will post a message on the front end when errors have occured. This helps debug as you write your scss.
+
+If you're working on a live/production site, you can send errors to a log. This will create a log file in your scss directory and print errors there as they occur. Just keep an eye on it, because the css will not be updated until errors have been resolved.
+
+#### Enqueuing
+The plugin can automatically add your css files to the header for you. This option will [enqueue](http://codex.wordpress.org/Function_Reference/wp_enqueue_style) all files found in the css directory defined in the settings. Keep this in mind if you have other non-compiled css files in this folder. The plugin will add them to the header, just don't reenque them somewhere else.
+
+Also keep in mind, that if you disable this plugin it can no longer enqueue files for you.
+
+## Directions
+*This plugin requires at least php 5.6 to work.*
+
+#### Importing Subfiles
+You can import other scss files into parent files and compile them into a single css file. To do this, use @import as normal in your scss file. All imported file names *must* start with an underscore. Otherwise they will be compiled into their own css file.
+
+When importing in your scss file, you can leave off the underscore.
+
+    @import 'subfile';
+
+#### Setting Variables via PHP
+You can set SCSS variables in your theme or plugin by using the wp_scss_variables filter.
+
+    function wp_scss_set_variables(){
+        $variables = array(
+            'black' => '#000',
+            'white' => '#fff'
+        );
+        return $variables;
+    }
+    add_filter('wp_scss_variables','wp_scss_set_variables');
+
+#### Always Recompile
+During development it's sometimes useful to force stylesheet compilation on every page load. Especially on hosts where filemtime() is not updating consistently.
+
+You can tell the plugin to always recompile in the plugin options page or by adding the following constant to your wp-config.php or functions.php file.
+
+    define('WP_SCSS_ALWAYS_RECOMPILE', true);
+
+#### Compass Support
+Currently there isn't a way to fully support [compass](https://github.com/chriseppstein/compass) with a php compiler. If you want limited support, you can manually import the compass framework. You'll need both the _compass.scss and compass directory.
+
+    compass / frameworks / compass / stylesheets /
+    @import 'compass';
+
+Alternatively, you can include [Bourbon](https://github.com/thoughtbot/bourbon) in a similar fashion.
+
+#### .sass Support
+This plugin will only work with .scss format.
+
+## Changelog
+* 2.1.3
+  * Must declare global to use it for $base_compiling_folder.
+* 2.1.2
+  * Correction for enqueueing styles not defaulting to get_stylesheet_directory() [Issue](https://github.com/ConnectThink/WP-SCSS/issues/168)
+* 2.1.1
+  * Bug fixes after merging 2.0.2 and 2.1.0 defaults worked, but new options did not. [Shadoath](https://github.com/ConnectThink/WP-SCSS/issues/165)
+* 2.1.0
+  * Settings dropdown added for choosing additional base compile locations outside of current theme. Suggestion by [pixeldesignstudio ](https://github.com/ConnectThink/WP-SCSS/issues/127)
+* 2.0.2
+  * Added option in settings to enable an 'always recompile' flag. Suggestion by [bick](https://github.com/ConnectThink/WP-SCSS/issues/151)
+* 2.0.1
+  * Bugfix to add filter for option_wpscss_options to remove Leafo if stored in DB. Thanks to [kinsky-org](https://github.com/ConnectThink/WP-SCSS/issues/157) for pointing this out
+  * Saving plugin settings will update DB with correct value.
+* 2.0.0
+  * Requires PHP 5.6
+  * Update src to use [ScssPHP github repo at 1.0.2](https://github.com/scssphp/scssphp/tree/1.0.2)
+  * Added check to make sure 'compiler' function was not already defined. [Shadoath](https://github.com/ConnectThink/WP-SCSS/pull/155)
+* 1.2.6
+  * Create cache dir if it doesn't exist [@XNBlank](https://github.com/ConnectThink/WP-SCSS/pull/135)
+  * Add cache dir as default [@mhbapcc](https://github.com/ConnectThink/WP-SCSS/pull/144)
+* 1.2.5
+  * Fix error when ".*" folders exist [@chameron](https://github.com/ConnectThink/WP-SCSS/pull/111)
+  * Add detailed error description for the directory settings [@andreyc0d3r](https://github.com/ConnectThink/WP-SCSS/pull/121)
+  * Fix on SASS compilation trigger [@fazzinipierluigi](https://github.com/ConnectThink/WP-SCSS/pull/122)
+* 1.2.4
+  * Updated scssphp to version 0.7.5
+  * Added source map [@iannacone](https://github.com/ConnectThink/WP-SCSS/issues/49)
+  * Always define $wpscss_compiler in the global scope [@jazbek](https://github.com/ConnectThink/WP-SCSS/pull/98)
+* 1.2.3
+  * Updated scssphp to version 0.7.2 [@hellerbenjamin](https://github.com/ConnectThink/WP-SCSS/pull/86)
+  * Removed depricated screen_icon()
+* 1.2.2
+  * Updated scssphp to version 0.6.6
+* 1.2.1
+  * Changed set version option to update if already exists
+* 1.2.0
+  * Fixed a bug where directory inputs were not getting sanitized [@mmcev106](https://github.com/ConnectThink/WP-SCSS/pull/66)
+  * Made the missing directory warning also display if a specified path is a file [@mmcev106](https://github.com/ConnectThink/WP-SCSS/pull/65)
+  * Added /vendor to .gitignore [@mmcev106](https://github.com/ConnectThink/WP-SCSS/pull/64)
+  * Dont enqueue already enqueued stylesheets [@bobbysmith007](https://github.com/ConnectThink/WP-SCSS/pull/61)
+* 1.1.9 - Added filter to set variables via PHP [@ohlle](https://github.com/ohlle) and option to minify CSS output [@mndewitt](https://github.com/mndewitt)
+* 1.1.8 - Various improvements from pull requests by [@jbrains](https://github.com/jbrains) and [@brainfork](https://github.com/brainfork)
+* 1.1.7 - Update scssphp to 0.0.12 - pull from #16 [@GabrielGil](https://github.com/GabrielGil)
+* 1.1.6 - Upgraded scss.inc.php to version 0.0.10 - pull from #12 [@kirkhoff](https://github.com/kirkhoff)
+* 1.1.5 - Added option to only show errors to logged in users - merge from #10 [@tolnem](https://github.com/tolnem)
+* 1.1.4 - Add support for subfolders in scss directory
+* 1.1.3 - Fix print bug (2) in header
+* 1.1.2 - Add support for moved wp-content directory
+* 1.1.1 - Catch permissions errors
+* 1.0.0 - Initial Release
+
+## License
+This plugin is developed and maintained by Connect Think and contributers.
+[GPL V3](http://www.gnu.org/copyleft/gpl.html)

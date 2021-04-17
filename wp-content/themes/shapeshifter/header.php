b6091f8aa8e9d7021428212baa4bef3ff2d53713
Bakchoy <shaia.bak@hotmail.com>
2021-04-16 18:06:34 -0700

    hompage content

--- wp-content/themes/shapeshifter/header.php
+++ wp-content/themes/shapeshifter/header.php
@@ -9,7 +9,7 @@
         <div class="row">
           <div class="col-md-4">
             <div class="header-logo">
-              <h1>LOGO</h1>
+              <a href="/"><h1>LOGO</h1></a>
             </div>
           </div>
           <div class="col-md-8">

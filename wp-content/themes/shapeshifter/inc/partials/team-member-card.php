b7358454f7fb013f39e89f77bd186ac9e18b12ec
victorbak <victor.bak2@gmail.com>
2021-04-21 23:43:09 -0700

    team member card skeleton

---
+++ wp-content/themes/shapeshifter/inc/partials/team-member-card.php
@@ -0,0 +1,18 @@
+<div class="team-member">
+    <div class="team-member-box">
+        <div class="team-member__photo">
+        </div>
+        <div class="team-member__identity">
+        </div>
+        <div class="underline"></div>
+        <div class="team-member__contact-info">
+            <p><em class="icon fas fa-phone mr-3"></em><?php //email ?></p>
+            <p><em class="icon far fa-envelope mr-3"></em><?php //linkedin ?></p>
+            <p><em class="icon fas fa-map-marker-alt mr-3"></em><?php //phone ?></p>
+        </div>
+        <div class="underline"></div>
+        <div class="team-member__bio">
+            <p></p>
+        </div>
+    </div>
+</div>

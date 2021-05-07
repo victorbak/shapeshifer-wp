48260b4d8192e52bb83622c4c32169b1b3ed843e
victorbak <victor.bak2@gmail.com>
2021-05-07 03:36:29 -0700

    Fixed mobile modal, Added mobile filters

--- wp-content/themes/shapeshifter/inc/partials/filters.php
+++ wp-content/themes/shapeshifter/inc/partials/filters.php
@@ -1,8 +1,26 @@
-<div class="filters">
-    <a data-filter="all" class="filter active">All</a>           
-    <?php foreach ( $args as $term ) :?>
-        <a data-filter="<?php echo $term->slug ?>" class="filter">
-            <?php echo $term->name ?>
-        </a>
-    <?php endforeach; ?>
+<div class="filters row">
+    <div class="d-none d-md-block col-md-12">
+        <a data-filter="all" class="filter active">All</a>           
+        <?php foreach ( $args as $term ) :?>
+            <a data-filter="<?php echo $term->slug ?>" class="filter">
+                <?php echo $term->name ?>
+            </a>
+        <?php endforeach; ?>
+    </div>
+    <div class="col-12 d-block d-md-none">
+        <button class="button" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            Filter by department
+            <span class="icon"><em class="fas fa-align-left"></em></span>
+        </button>
+        <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+                <a data-filter="all" class="filter active">All</a>           
+                <?php foreach ( $args as $term ) :?>
+                    <a data-filter="<?php echo $term->slug ?>" class="filter">
+                        <?php echo $term->name ?>
+                    </a>
+                <?php endforeach; ?>        
+            </div>
+        </div>
+    </div>
 </div>
--- wp-content/themes/shapeshifter/inc/partials/modal.php
+++ wp-content/themes/shapeshifter/inc/partials/modal.php
@@ -5,16 +5,20 @@
 <!-- <div class="modal fade" id="modalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered container" role="document">
         <div class="modal-content">  -->
-            <div class="ss-modal px-4">
-                <div class="ss-modal__header pl-3 pr-5 py-md-1">
-                    <h5 class='ss-modal__header--title'> <?php the_title(); ?> </h5>
-                    <p class="ss-modal__header--date"><span><em class="far fa-clock mr-1"></em></span>Date Posted: </p>
+            <div class="ss-modal">
+                <div class="ss-modal-content row">
+                    <div class="ss-modal__header col-xs-12 pl-3 pr-5 py-md-1">
+                        <h5 class='ss-modal__header--title'> <?php the_title(); ?> </h5>
+                        <?php echo "<p class='ss-modal__header--date'><span><em class='far fa-clock mr-1'></em></span>Date Posted: {$args['date_posted']}</p>"; ?>
+                    </div>
+                    <div class="content py-xs-2">
+                        <p class='description col-xs-12'><?php the_content() ?></p>
+                    </div>
+                    <a class="cta py-2 px-4" href="https://indeed.com" target="_blank">
+                        <span>Apply Now</span>
+                        <em class="icon fas fa-chevron-right"></em>
+                    </a>
                 </div>
-                <p class='description'><?php the_content() ?></p>
-                <a class="cta py-2 px-4" href="https://indeed.com" target="_blank">
-                  <span>Apply Now</span>
-                  <em class="icon fas fa-chevron-right"></em>
-                </a>
             </div>
         <!-- </div>
     </div>

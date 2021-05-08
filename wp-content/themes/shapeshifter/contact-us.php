c01332e38888aa934f56ee9f53d2ee5b9e09ac5a
victorbak <victor.bak2@gmail.com>
2021-05-08 03:19:54 -0700

    Fixed contact page content

--- wp-content/themes/shapeshifter/contact-us.php
+++ wp-content/themes/shapeshifter/contact-us.php
@@ -8,13 +8,28 @@
     <?php get_header(); ?>
 <!-- TODO: ADD ICONS -->
 
+<?php $contact_us = get_field('contact-button-link'); var_dump($contact_us);?>
+<?php $google_maps = get_field('google-maps-url'); ?>
+
     <!-- Content here -->
         <div class="contact-us page">
-            <?php get_template_part( 'inc/sections/content-contact-block' ); ?>
+            <section class="container px-4 py-5">
+                <div class="row">
+                    <div class="content col-md-12 col-lg-7 mb-xs-4 mb-lg-0 py-lg-4">
+                        <?php the_field('contact-intro');?>
+                        <a <?php echo "href='" . esc_url($contact_us) . "'"; ?> class="button mt-5">Contact Us <span class="icon"><em class="fas fa-chevron-right"></em></span></a>
+                    </div>
+                    <div class="col-md-12 col-lg-5 col-xl-4 offset-xl-1 py-4">
+                        <?php get_template_part( 'inc/partials/contact-info-card' ); ?>
+                        
+                    </div>
+                </div>
+            </section>
             <section class="container-fluid py-md-4 py-lg-6 px-0">
+                <?php var_dump(the_field('google-maps-url')); ?>
                 <div class="map-banner">
                     <div class="overlay">
-                    <a href="http://google.ca/maps" target="_blank" >
+                    <a <?php echo "href='" . esc_url($google_maps) . "'"; ?>"target="_blank" >
                         <div class="row mx-0">
                             <div class="col-md-4">
                             </div>

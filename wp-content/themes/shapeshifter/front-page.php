a02302e49036db065d817f30f96c1dbc01921253
Bakchoy <shaia.bak@hotmail.com>
2021-05-07 22:51:42 -0700

    home projects2

--- wp-content/themes/shapeshifter/front-page.php
+++ wp-content/themes/shapeshifter/front-page.php
@@ -8,35 +8,47 @@ $featured_project3 = get_field('featured_project_3');
 <div class="home page">
     <section class="project-section">
         <?php if($featured_project1): ?>
-            <?php //foreach( $featured_project1 as $project1 ): ?>
-                <?php
-                    $projectName = get_field( 'project_name', $featured_project1->ID );
-                    $projectImage = get_field( 'gallery', $featured_project1->ID );
-                ?>
-                <div class="container-fluid px-0 project-container">
-                    <a href="/coming-soon">
-                        <h2 class="project-title"><?php echo esc_html( $projectName ); ?></h2>
-                        <img src="<?php echo esc_url($projectImage['url']); ?>" alt="<?php echo esc_url($projectImage['alt']); ?>">
-                        <div class="project-release-banner">
-                            Newest Release
-                        </div>
-                    </a>
-                </div>
-            <?php //endforeach; ?>
-        <?php endif; ?>
-        <?php if($featured_project2): ?>
+            <?php
+                $projectName = get_field( 'project_name', $featured_project1->ID );
+                $projectImage = get_field( 'gallery', $featured_project1->ID );
+            ?>
             <div class="container-fluid px-0 project-container">
                 <a href="/coming-soon">
-                    <h2 class="project-title">Project One</h2>
-                    <img src="<?php bloginfo('template_url'); ?>/assets/projects/project1-2x.png" alt="project1">
+                    <h2 class="project-title"><?php echo esc_html( $projectName ); ?></h2>
+                    <img src="<?php echo esc_url($projectImage['url']); ?>" alt="<?php echo esc_url($projectImage['alt']); ?>">
+                    <div class="project-release-banner">
+                        Newest Release
+                    </div>
+                </a>
+            </div>
+        <?php endif; ?>
+        <?php if($featured_project2): ?>
+            <?php
+                $projectName = get_field( 'project_name', $featured_project2->ID );
+                $projectImage = get_field( 'gallery', $featured_project2->ID );
+            ?>
+            <div class="container-fluid px-0 project-container">
+                <a href="/coming-soon">
+                    <h2 class="project-title"><?php echo esc_html( $projectName ); ?></h2>
+                    <img src="<?php echo esc_url($projectImage['url']); ?>" alt="<?php echo esc_url($projectImage['alt']); ?>">
+                    <div class="project-release-banner">
+                        Newest Release
+                    </div>
                 </a>
             </div>
         <?php endif; ?>
         <?php if($featured_project3): ?>
+            <?php
+                $projectName = get_field( 'project_name', $featured_project3->ID );
+                $projectImage = get_field( 'gallery', $featured_project3->ID );
+            ?>
             <div class="container-fluid px-0 project-container">
                 <a href="/coming-soon">
-                    <h2 class="project-title">Project One</h2>
-                    <img src="<?php bloginfo('template_url'); ?>/assets/projects/project1-2x.png" alt="project1">
+                    <h2 class="project-title"><?php echo esc_html( $projectName ); ?></h2>
+                    <img src="<?php echo esc_url($projectImage['url']); ?>" alt="<?php echo esc_url($projectImage['alt']); ?>">
+                    <div class="project-release-banner">
+                        Newest Release
+                    </div>
                 </a>
             </div>
         <?php endif; ?>

4c5abf4b9b5e60f8baf7073330662806e69736b9
victorbak <victor.bak2@gmail.com>
2021-05-08 02:11:19 -0700

    what have i done...

--- wp-content/themes/shapeshifter/header.php
+++ wp-content/themes/shapeshifter/header.php
@@ -3,7 +3,47 @@
   <head>
     <?php wp_head();?>
 </head>
-  <body <?php body_class();?>>
+<script>
+  $(document).ready(function() {
+    function fadePreloader() {
+      let preloaderFadeOutTime = 600;
+      var preloader = $('.loading');
+      preloader.show().delay(600).fadeOut(preloaderFadeOutTime);
+  }
+  fadePreloader();
+  });
+</script>
+<div class="loading" 
+    style="
+        visibility: hidden;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #111;
+        z-index: 999999;">
+    <div class="container" 
+    style="height: 100%;
+        display: flex;
+        justify-content: center;">
+        <div class="loading-content" 
+        style="width: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;">
+            <div class="logo" style="text-align: center;">
+            <img class="mr-2" src="<?php bloginfo('template_url'); ?>/assets/Logo_Vector.svg" alt="Shapeshifter">
+            </div>
+            <div class="container-fluid">
+                <div class="full-divider"></div>
+            </div>
+            <h1 class="text" style="text-align: center;">LOADING</h1>
+        </div>
+    </div>
+  </div>
+  <body <?php body_class();?>>  
     <section class="header">
       <div class="container-fluid header-container">
         <div class="row">
@@ -24,4 +64,11 @@
       </div>
     </section>
   </body>
+  <script>  
+  var currentLocation = window.location.pathname;
+  if(currentLocation == "/") {
+    $('.loading').css('visibility', '');
+    console.log($('.loading'));
+  }
+  </script>
 </html>

accc0a7e33a0a4991acd1420301a97e8ee2616a8
victorbak <victor.bak2@gmail.com>
2021-05-14 02:09:35 -0700

    Conditional logic for team cards and contact card

--- wp-content/themes/shapeshifter/inc/partials/contact-info-card.php
+++ wp-content/themes/shapeshifter/inc/partials/contact-info-card.php
@@ -2,7 +2,6 @@
     $post = get_page_by_title('Contact Info', OBJECT, 'reusable_block'); 
     $fields = get_fields();
 ?>
-<!-- TODO: ADD ICONS -->
 <main class="contact-info">
     <div class="box">
         <div class="triangle"></div>
@@ -10,21 +9,36 @@
             <h5 class="contact-info--header">CONTACT INFO</h5>
             <div class="divider"></div>
             <div class="p-2">
+            <?php if ( ! empty($fields['phone_number'] ) ): ?>
                 <p><em class="icon fas fa-phone mr-3"></em><?php echo $fields['phone_number']; ?></p>
+            <?php endif ?>
+            <?php if ( ! empty($fields['email'] ) ): ?>
                 <p><em class="icon far fa-envelope mr-3"></em><?php echo $fields['email']; ?></p>
+            <?php endif ?>
+            <?php if ( ! empty($fields['street_address'] ) ): ?>
                 <p><em class="icon fas fa-map-marker-alt mr-3"></em><?php echo $fields['street_address']; ?></p>
+            <?php endif ?>
             </div>
         </div>
         <div>
             <h5 class="contact-info--header">SOCIAL</h5>
             <div class="divider"></div>
             <div class="row p-2">
-                <!-- Add conditionals -->
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-twitter"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-facebook-f"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-vimeo"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-youtube"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-linkedin-in"></em></a>
+            <?php if ( ! empty($fields['twitter_url'] ) ): ?>
+                <a href="<?php echo $fields['twitter_url'] ?>" target="blank"><em class="icon col-1 fab fa-twitter"></em></a>
+            <?php endif ?>
+            <?php if ( ! empty($fields['facebook_url'] ) ): ?>
+                <a href="<?php echo $fields['facebook_url'] ?>" target="blank"><em class="icon col-1 fab fa-facebook-f"></em></a>
+            <?php endif ?>
+            <?php if ( ! empty($fields['facebook_url'] ) ): ?>
+                <a href="<?php echo $fields['vimeo_url'] ?>" target="blank"><em class="icon col-1 fab fa-vimeo"></em></a>
+            <?php endif ?>
+            <?php if ( ! empty($fields['youtube_url'] ) ): ?>
+                <a href="<?php echo $fields['youtube_url'] ?>" target="blank"><em class="icon col-1 fab fa-youtube"></em></a>
+            <?php endif ?>
+            <?php if ( ! empty($fields['linkedin_url'] ) ): ?>
+                <a href="<?php echo $fields['linkedin_url'] ?>" target="blank"><em class="icon col-1 fab fa-linkedin-in"></em></a>
+            <?php endif ?>
             </div>
         </div>
     </div>

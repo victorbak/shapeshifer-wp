90137fe7cd46c924b98c4b4b6e84a83afd38e759
Bakchoy <shaia.bak@hotmail.com>
2021-05-16 17:54:00 -0700

    mobile: add close on filter choice

--- wp-content/themes/shapeshifter/inc/partials/filters.php
+++ wp-content/themes/shapeshifter/inc/partials/filters.php
@@ -8,15 +8,15 @@
         <?php endforeach; ?>
     </div>
     <div class="col-12 d-block d-lg-none">
-        <button class="button" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        <button class="button" type="button" data-toggle="collapse" data-target="#deptFilter" aria-expanded="false" aria-controls="collapseExample">
             Filter by department
             <span class="icon"><em class="fas fa-align-left"></em></span>
         </button>
-        <div class="collapse" id="collapseExample">
+        <div class="collapse" id="deptFilter">
             <div class="card card-body">
-                <a data-filter="all" class="filter active">All</a>           
+                <a data-target="#deptFilter" data-toggle="collapse" data-toggle="collapse" data-filter="all" class="filter active">All</a>           
                 <?php foreach ( $args as $term ) :?>
-                    <a data-filter="<?php echo $term->slug ?>" class="filter">
+                    <a data-target="#deptFilter" data-toggle="collapse" data-filter="<?php echo $term->slug ?>" class="filter">
                         <?php echo $term->name ?>
                     </a>
                 <?php endforeach; ?>        

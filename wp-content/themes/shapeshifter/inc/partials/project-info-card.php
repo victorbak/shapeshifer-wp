4742ff1c9b25806fbcf58c11e66826ce35d3ece1
Victor Bak <victor.bak2@gmail.com>
2021-06-19 04:11:45 -0700

    Added logic for software icons

--- wp-content/themes/shapeshifter/inc/partials/project-info-card.php
+++ wp-content/themes/shapeshifter/inc/partials/project-info-card.php
@@ -1,8 +1,20 @@
 <?php 
     $post = get_page_by_title('Contact Info', OBJECT, 'projects'); 
     $fields = get_fields();
+
+    $software_header = $fields['software_section_heading'];
+    $software_used = $fields['software_used'];
+
+    $software_icons = array(
+      'unreal_engine' => '',
+      'xsens'         => '',
+      'nuke'          => '',
+      'vray'          => '',
+      'maya'          => get_theme_file_uri( '/assets/icons/maya.png' ),
+      '3ds_max'       => get_theme_file_uri( '/assets/icons/3ds_max.png' ),
+      'autodesk'      => get_theme_file_uri( '/assets/icons/maya.png' ),
+    );
 ?>
-<!-- TODO: ADD ICONS -->
 <main class="contact-info">
     <div class="box">
         <div class="triangle"></div>
@@ -13,16 +25,21 @@
                 <p><?php echo $fields['artists']; ?></p>
             </div>
         </div>
-        <!-- <div>
-            <h5 class="contact-info--header">FEATURED WORK ON</h5>
-            <div class="divider"></div>
-            <div class="row p-2">
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-twitter"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-facebook-f"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-vimeo"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-youtube"></em></a>
-                <a href="https://google.ca" target="blank"><em class="icon col-1 fab fa-linkedin-in"></em></a>
-            </div>
-        </div> -->
+        <?php if ( ! empty( $software_used ) ) : ?>
+          <div>
+              <h5 class="contact-info--header"><?php echo $software_header; ?></h5>
+              <div class="divider"></div>
+              <div class="software-container row p-2">
+                <?php foreach( $software_used as $software ) : ?>
+                  <?php if ( array_key_exists( $software, $software_icons ) && ! empty( $software_icons[$software] ) ): ?>
+                    <img class="software-icon" 
+                      src="<?php echo esc_url( $software_icons[$software] ); ?>" 
+                      alt="<?php echo esc_attr( $software ) . ' ' . 'software icon' ?>"
+                      title="<?php echo esc_attr( str_replace(' ', '_', ucfirst( $software ) ) ) ?>" />
+                  <?php endif ?>
+                <?php endforeach ?>
+              </div>
+          </div>
+        <?php endif; ?>
     </div>
 </main>

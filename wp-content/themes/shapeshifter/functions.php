71ba95bc34a947db07ba348471630060ced6e22e
Victor Bak <victor.bak2@gmail.com>
2021-06-28 16:24:19 -0700

    Removed gutenburg css

--- wp-content/themes/shapeshifter/functions.php
+++ wp-content/themes/shapeshifter/functions.php
@@ -65,6 +65,14 @@ function admin_styles() {
 }
 add_action('admin_head', 'admin_styles');
 
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
 
 /* -INCLUDES------------------------------------------------- */
 // require_once 'inc/autoload.php';
